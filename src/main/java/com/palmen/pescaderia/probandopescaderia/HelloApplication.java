89a0d905b73694077c02bac660400d23eaee9eaa
Pbl99 <pabloam1999@hotmail.com>
2024-04-05 12:08:16 +0200

    Añado la vista principal StockPescaderia con sus imagenes respecticas y el controlador

--- src/main/java/com/palmen/pescaderia/probandopescaderia/HelloApplication.java
+++ src/main/java/com/palmen/pescaderia/probandopescaderia/HelloApplication.java
@@ -10,9 +10,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoguinPescaderia.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 747, 365);
-        stage.setTitle("Gestionar Pescaderia");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("StockPescaderia.fxml"));
+        //Scene scene = new Scene(fxmlLoader.load(), 747, 365);
+        Scene scene = new Scene(fxmlLoader.load(), 950, 600);
+        // Este scene para aproximar tamaño de la vista StockPescaderia
+
+        stage.setTitle("Administador Stock Pescaderia");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
---
+++ src/main/java/com/palmen/pescaderia/probandopescaderia/controllers/StockController.java
@@ -0,0 +1,92 @@
+package com.palmen.pescaderia.probandopescaderia.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+public class StockController {
+
+    @FXML
+    private Button btnUsuario;
+
+    @FXML
+    private ImageView imgBacalao;
+
+    @FXML
+    private ImageView imgBoqueron;
+
+    @FXML
+    private ImageView imgDorada;
+
+    @FXML
+    private ImageView imgGamba;
+
+    @FXML
+    private ImageView imgLenguado;
+
+    @FXML
+    private ImageView imgLubina;
+
+    @FXML
+    private ImageView imgMerluza;
+
+    @FXML
+    private ImageView imgPulpo;
+
+    @FXML
+    private ImageView imgSalmon;
+
+    @FXML
+    private ImageView imgSardina;
+
+    @FXML
+    private ImageView imgSepia;
+
+    @FXML
+    private ImageView imgTrucha;
+
+    @FXML
+    private Label lblBacalao;
+
+    @FXML
+    private Label lblBoqueron;
+
+    @FXML
+    private Label lblDorada;
+
+    @FXML
+    private Label lblGamba;
+
+    @FXML
+    private Label lblLenguado;
+
+    @FXML
+    private Label lblLubina;
+
+    @FXML
+    private Label lblMerluza;
+
+    @FXML
+    private Label lblPulpo;
+
+    @FXML
+    private Label lblSalmon;
+
+    @FXML
+    private Label lblSardina;
+
+    @FXML
+    private Label lblSepia;
+
+    @FXML
+    private Label lblTrucha;
+
+    @FXML
+    private Pane panelInfo;
+
+    @FXML
+    private Pane panelPrincipal;
+
+}
